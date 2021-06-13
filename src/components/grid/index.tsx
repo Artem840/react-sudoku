0ec6a616144d0a15f1e9dbd24a8c8a1177e01f6f
Artem Gumarov <agumarov@37bytes.com>
2021-06-13 12:11:56 +0300

    WIP

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,95 @@
+import { Children, FC, useCallback, useEffect } from "react";
+import useMousetrap from 'react-hook-mousetrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { AnyAction, Dispatch } from 'redux'
+
+import { createGrid, IReducer, selectBlock } from 'reducers'
+import { BLOCK_COORDS, INDEX } from "typings"
+
+import Block from './block'
+import { Container, Row } from './styles'
+
+interface IState {
+    selectedBlock?: BLOCK_COORDS
+}
+
+const Grid: FC = () => {
+    const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
+        selectedBlock
+    }))
+    const dispatch = useDispatch<Dispatch<AnyAction>>()
+    const create = useCallback(() => dispatch(createGrid()), [dispatch])
+    useEffect(() => {
+        create()
+    }, [create])
+    
+    function moveDown() {
+        if (state.selectedBlock && state.selectedBlock[0] < 8) {
+            dispatch(
+                selectBlock([
+                    (state.selectedBlock[0] + 1) as INDEX,
+                    state.selectedBlock[1]
+                ])
+            )
+        }
+    }
+
+    function moveLeft() {
+        if (state.selectedBlock && state.selectedBlock[1] > 0) {
+            dispatch(
+                selectBlock([
+                    state.selectedBlock[0],
+                    (state.selectedBlock[1] - 1) as INDEX
+                ])
+            )
+        }
+    }
+
+    function moveRight() {
+        if (state.selectedBlock && state.selectedBlock[1] < 8) {
+            dispatch(
+                selectBlock([
+                    state.selectedBlock[0],
+                    (state.selectedBlock[1] + 1) as INDEX
+                ])
+            )
+        }
+    }
+
+    function moveUp() {
+        if (state.selectedBlock && state.selectedBlock[0] > 0) {
+            dispatch(
+                selectBlock([
+                    (state.selectedBlock[0] - 1) as INDEX,
+                    state.selectedBlock[1]
+                ])
+            )
+        }
+    }    
+
+    useMousetrap('down', moveDown)
+    useMousetrap('left', moveLeft)
+    useMousetrap('right', moveRight)
+    useMousetrap('up', moveUp)
+
+    return (
+        <Container data-cy="grid-container">
+            {Children.toArray(
+                [...Array(9)].map((_, rowIndex) => (
+                    <Row data-cy="grid-row-container">
+                        {Children.toArray(
+                            [...Array(9)].map((_, colIndex) => (
+                                <Block
+                                    colIndex={colIndex as INDEX}
+                                    rowIndex={rowIndex as INDEX} 
+                                />
+                            ))
+                        )}
+                    </Row>
+                ))
+            )}
+        </Container>
+    )
+}
+
+export default Grid
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+
+import { Content, Title, Card, Grid } from 'components'
+import { configureStore, unregister } from 'core';
+import { GlobalStyles, theme } from 'styles'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Provider store={store}>
+        <Content data-cy="content">
+          <Title data-cy="title">Sudoku</Title>
+          <Card data-cy="card">
+            <Grid />
+          </Card>
+        </Content>
+      </Provider>
+    </ThemeProvider>
+  </StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+unregister();
