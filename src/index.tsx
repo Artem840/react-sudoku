c14dfb7493019659f49ead19d6a0d293298072af
Artem <artemgumarov840@gmail.com>
2021-06-15 00:04:19 +0500

    completed app

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,34 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { ThemeProvider } from 'styled-components'
 
-import { Content, Title, Card, Grid } from 'components'
-import { configureStore, unregister } from 'core';
+import { Card, Content, Grid, NewButton, Numbers, Title } from 'components'
+import { configureStore, register } from 'core';
 import { GlobalStyles, theme } from 'styles'
 
-const store = configureStore()
+const { persistor, store } = configureStore()
 
 ReactDOM.render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Provider store={store}>
-        <Content data-cy="content">
-          <Title data-cy="title">Sudoku</Title>
-          <Card data-cy="card">
-            <Grid />
-          </Card>
-        </Content>
+        <PersistGate loading={null} persistor={persistor}>
+          <Content data-cy="content">
+            <Title data-cy="title">Sudoku</Title>
+            <Card data-cy="card">
+              <NewButton />
+              <Grid />
+              <Numbers />
+            </Card>
+          </Content>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
 );
 
-unregister();
+register();
